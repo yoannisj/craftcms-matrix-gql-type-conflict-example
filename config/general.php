50e82cbf800976a86da1466db8100652bc1692e4
Yoannis Jamar <yoannis@Yoanniss-MBP.lan>
2024-11-05 12:57:37 +0000

    setup craft with example data-structure

---
+++ config/general.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+use craft\config\GeneralConfig;
+use craft\helpers\App;
+
+return GeneralConfig::create()
+    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
+    ->defaultWeekStartDay(1)
+    // Prevent generated URLs from including "index.php"
+    ->omitScriptNameInUrls()
+    // Preload Single entries as Twig variables
+    ->preloadSingles()
+    // Prevent user enumeration attacks
+    ->preventUserEnumeration()
+    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->aliases([
+        '@siteUrl' => App::env('PRIMARY_SITE_URL'),
+        '@webroot' => App::env('CRAFT_WEB_ROOT') ?: (dirname(__DIR__) . '/web'),
+    ])
+;
